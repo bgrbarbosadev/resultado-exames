216ae9494e53f5bbc2d0ceae797ec8880b74bd8b
bgrbarbosa.dev <bgrbarbosa.dev@gmail.com>
2025-10-01 16:00:24 -0300

    ms-registry criado

---
+++ ms-registry/src/test/java/br/com/bgrbarbosa/ms_registry/MsRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bgrbarbosa.ms_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
